adf26beac187dcb71fe901132bdeae0a24ead71c
darrenak403 <ngothanhdat4002@gmail.com>
2025-10-24 17:54:56 +0700

    Add initial project structure for Order API solution
    Created solution file and added projects for Domain, Application, Infrastructure, and Presentation layers. Configured .NET 8.0 targeting, package references, and logging settings. Implemented Order entity and set up web application with Swagger support. Defined launch settings for development environment.

---
+++ MicroserviceProject/ECommerce.OrderApiSolution/OrderApi.Domain/Entities/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderApi.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ClientId { get; set; }
+        public int PurchaseQuantity { get; set; }
+        public DateTime OrderedData { get; set; } = DateTime.UtcNow;
+    }
+}
