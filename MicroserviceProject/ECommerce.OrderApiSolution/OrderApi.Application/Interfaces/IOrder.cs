c0d6ad27bc9c1a8ee598b0c2875d949c9ab96ac9
darrenak403 <ngothanhdat4002@gmail.com>
2025-10-24 18:17:20 +0700

    Add IOrder interface and update project structure
    Updated `OrderApi.Application.csproj` to include a folder reference for "Services\".
    
    Introduced `IOrder` interface in `IOrder.cs` with a method `GetOrdersAsync` for asynchronous retrieval of orders based on a predicate.

---
+++ MicroserviceProject/ECommerce.OrderApiSolution/OrderApi.Application/Interfaces/IOrder.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+using ECommerce.ShareLibrary.Interface;
+using OrderApi.Domain.Entities;
+
+namespace OrderApi.Application.Interfaces
+{
+    public interface IOrder : IGenericInterface<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersAsync(Expression<Func<Order, bool>> predicate);
+    }
+}
