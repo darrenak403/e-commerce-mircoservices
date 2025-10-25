c04946da747513a600a111319006682865b2baa2
darrenak403 <ngothanhdat4002@gmail.com>
2025-10-25 18:19:37 +0700

    Add IUser interface for user management operations
    Introduces the `IUser` interface in the
    `AuthenticationApi.Application.Interfaces` namespace.
    Defines asynchronous methods for user registration,
    login, and retrieval of user information.
    Includes necessary using directives for
    `AppUserDTO` and `Response` types.

---
+++ MicroserviceProject/ECommerce.AuthenticationApiSolution/AuthenticationApi.Application/Interfaces/IUser.cs
@@ -0,0 +1,13 @@
+﻿using AuthenticationApi.Application.DTOs;
+using ECommerce.ShareLibrary.Responses;
+
+namespace AuthenticationApi.Application.Interfaces
+{
+    public interface IUser
+    {
+        Task<Response> Register(AppUserDTO appUserDTO);
+        Task<Response> Login(LoginDTO loginDTO);
+        Task<AppUserDTO> GetUser(int userId);
+
+    }
+}
