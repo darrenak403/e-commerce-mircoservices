7b9298afdeeb3345f5119ec83d992dbd0fb5d636
darrenak403 <ngothanhdat4002@gmail.com>
2025-10-25 18:56:33 +0700

    Update launch settings and add service container
    Modified `launchSettings.json` to change the HTTP and HTTPS `applicationUrl` to `http://localhost:5000` and removed the HTTP configuration block.
    
    Introduced a new `ServiceContainer` class in `ServiceContainer.cs` with methods for adding infrastructure services and registering middleware policies, enhancing dependency injection and application configuration.

---
+++ MicroserviceProject/ECommerce.AuthenticationApiSolution/AuthenticationApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,35 @@
+﻿using AuthenticationApi.Application.Interfaces;
+using AuthenticationApi.Infrastructure.Data;
+using AuthenticationApi.Infrastructure.Repositories;
+using ECommerce.ShareLibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthenticationApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Add database connectivity
+            //JWT Add Authentication Scheme
+            SharedServiceContainer.AddShareServices<AuthenticationDbContext>(services, configuration, configuration["MySerilog:FileName"]!);
+
+            //Create Dependency Injection
+            services.AddScoped<IUser, UserRepository>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UserInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            //Register middleware such as:
+            //Global Exception: Handle external errors
+            //Listen Only To Api Gateway: block all outsides call
+            SharedServiceContainer.UseSharedPolicies(app);
+
+            return app;
+        }
+    }
+}
