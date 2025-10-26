663cc035e43cf302c4a7e80f823e8cc9d5eff245
darrenak403 <ngothanhdat4002@gmail.com>
2025-10-26 21:59:10 +0700

    Update middleware and configuration settings
    - Added new namespaces in `Program.cs` for Ocelot and ECommerce.
    - Modified `AddCors` to allow any method and origin.
    - Adjusted middleware order: moved `app.UseAuthentication()` and added `app.UseAuthorization()`.
    - Updated logging level for Ocelot to `Debug` in `appsettings.json`.
    - Changed authentication key in `appsettings.json`.

--- MicroserviceProject/ECommerce.ApiGatewaySolution/ApiGateway.Presentation/Program.cs
+++ MicroserviceProject/ECommerce.ApiGatewaySolution/ApiGateway.Presentation/Program.cs
@@ -1,12 +1,12 @@
-using ApiGateway.Presentation.Middleware;
+﻿using ApiGateway.Presentation.Middleware;
 using ECommerce.ShareLibrary.DependencyInjection;
 using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
-//
+// Cấu hình
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot().AddCacheManager(x => x.WithDictionaryHandle());
 JWTAuthenticationScheme.AddJWTAuthenticationScheme(builder.Services, builder.Configuration);
@@ -20,17 +20,14 @@ builder.Services.AddCors(options =>
     });
 });
 
-//
 var app = builder.Build();
+
+// Thứ tự middleware rất quan trọng
 app.UseHttpsRedirection();
-//
 app.UseCors();
-app.UseAuthentication();
 app.UseMiddleware<AttachSignatureToRequest>();
-app.UseOcelot().Wait();
-//
-
-
-
+app.UseAuthentication(); // Thêm dòng này
+app.UseAuthorization();  // Thêm dòng này
+await app.UseOcelot();
 
 app.Run();
